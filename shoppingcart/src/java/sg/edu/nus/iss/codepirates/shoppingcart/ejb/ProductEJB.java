4331a72038e1cd7e11befe74c29476a962672a78
Divahar Sethuraman <e0146870@u.nus.edu>
2017-10-26 17:04:46 +0800

    Added EJB, JSF, Primefaces and model

---
+++ shoppingcart/src/java/sg/edu/nus/iss/codepirates/shoppingcart/ejb/ProductEJB.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.edu.nus.iss.codepirates.shoppingcart.ejb;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import sg.edu.nus.iss.codepirates.shoppingcart.model.Product;
+
+/**
+ *
+ * @author Divahar Sethuraman
+ * This class contains the persistence logic for Product
+ * 
+ */
+
+@Stateless
+public class ProductEJB {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Product> getProductDetails() {
+        return em.createQuery("select prod from Product prod")
+                .getResultList();
+    }
+
+    public byte[] getProdImage(String pid) {
+        Product prod = em.find(Product.class, pid);
+        return prod.getProductImage();
+    }
+}
---
+++ shoppingcart/src/java/sg/edu/nus/iss/codepirates/shoppingcart/model/Product.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sg.edu.nus.iss.codepirates.shoppingcart.model;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author Divahar Sethuraman
+ * This class is the entity class for table Product
+ */
+
+@Entity
+@Table(name = "product")
+public class Product implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id @Column(name = "productId")
+    private String productId;
+    
+    @Column(name = "productName")
+    private String productName;
+       
+    @Transient
+    private int quantity;
+    
+    @Column(name = "productImage")
+    @Lob
+    private byte[] productImage;
+    
+    @Column(name = "available")
+    private int available;
+
+    public byte[] getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(byte[] productImage) {
+        this.productImage = productImage;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }       
+    
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {        
+        this.quantity = quantity;
+    }  
+    
+}
