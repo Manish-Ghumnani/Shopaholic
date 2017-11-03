af71cbec04a17f095d046428fa2628e45eb39301
Divahar <e0146870@u.nus.edu>
2017-11-04 01:21:15 +0800

    Removed login

--- shoppingcart/src/java/sg/edu/nus/iss/codepirates/shoppingcart/bean/UserLoginBean.java
+++ shoppingcart/src/java/sg/edu/nus/iss/codepirates/shoppingcart/bean/UserLoginBean.java
@@ -32,20 +32,7 @@ public class UserLoginBean {
         this.password = password;
     }
 
-    public void login(ActionEvent event) {
-        RequestContext context = RequestContext.getCurrentInstance();
-        FacesMessage message = null;
-        boolean loggedIn = false;
-
-        if (username != null && username.equals("admin") && password != null && password.equals("admin")) {
-            loggedIn = true;
-            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
-        } else {
-            loggedIn = false;
-            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Loggin Error", "Invalid credentials");
-        }
-
-        FacesContext.getCurrentInstance().addMessage(null, message);
-        context.addCallbackParam("loggedIn", loggedIn);
-    }
+    public String login() {
+        return "shopping";
+}
 }
