c4794cedf54e545ecc14f8a48267e84fe390fd65
prasanna12510 <prasanna.pawar43@gmail.com>
2017-11-06 12:34:47 +0800

    Commited Register User

--- snapNshare/src/java/sg/edu/nus/iss/codepirates/snapNshare/service/RegisterService.java
+++ snapNshare/src/java/sg/edu/nus/iss/codepirates/snapNshare/service/RegisterService.java
@@ -5,14 +5,20 @@
  */
 package sg.edu.nus.iss.codepirates.snapNshare.service;
 
+import java.sql.Date;
+import javax.ejb.EJB;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import sg.edu.nus.iss.codepirates.snapNshare.ejb.RegisterEJB;
+import sg.edu.nus.iss.codepirates.snapNshare.entity.User;
 
 /**
  * REST Web Service
@@ -24,11 +30,17 @@ public class RegisterService {
 
     @Context
     private UriInfo context;
+    
+    private User newUser;
+    
+    @EJB
+    RegisterEJB register;
 
     /**
      * Creates a new instance of UserResource
      */
     public RegisterService() {
+        newUser = new User();
     }
 
     /**
@@ -43,10 +55,16 @@ public class RegisterService {
 
     /**
      * PUT method for updating or creating an instance of UserResource
-     * @param content representation for the resource
+     * @param username
      */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    @POST
+    @Path("/{username}")
+    //@Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(@PathParam("username") String username) {
+        
+        newUser.setUserId(username);
+        newUser.setCreatedAt(new Date(System.currentTimeMillis()));
+        
+        register.createUser(newUser);
     }
 }
