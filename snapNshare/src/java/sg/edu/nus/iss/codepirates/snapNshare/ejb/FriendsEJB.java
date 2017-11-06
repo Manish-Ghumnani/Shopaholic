02cda83cfbf46817133c651e6f07576cf9117d90
Manish-Ghumnani <maniac3493@gmail.com>
2017-11-06 13:56:30 +0800

    retrieving friends list as JsonArray

--- snapNshare/src/java/sg/edu/nus/iss/codepirates/snapNshare/ejb/FriendsEJB.java
+++ snapNshare/src/java/sg/edu/nus/iss/codepirates/snapNshare/ejb/FriendsEJB.java
@@ -5,9 +5,14 @@
  */
 package sg.edu.nus.iss.codepirates.snapNshare.ejb;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import sg.edu.nus.iss.codepirates.snapNshare.entity.Friends;
+import sg.edu.nus.iss.codepirates.snapNshare.entity.FriendsId;
 
 /**
  *
@@ -21,6 +26,19 @@ public class FriendsEJB {
     @PersistenceContext
     private EntityManager em;
     
+    public List<String> getFriends(String userName)
+    {
+        String queryString = "select f from Friends f where (f.friendsId.friendRef = :user)";
+        TypedQuery<Friends> query = em.createQuery(queryString, Friends.class);
+        query.setParameter("user", userName);
+        
+        List<Friends> friends = query.getResultList();
+        List<String> friendNames = new ArrayList<String>();
+        friends.forEach((f) -> {
+            friendNames.add(f.getFriendsId().getFriendId());
+        });
+        return friendNames;
+    }
     
 
 }
--- snapNshare/src/java/sg/edu/nus/iss/codepirates/snapNshare/service/FriendsService.java
+++ snapNshare/src/java/sg/edu/nus/iss/codepirates/snapNshare/service/FriendsService.java
@@ -5,6 +5,15 @@
  */
 package sg.edu.nus.iss.codepirates.snapNshare.service;
 
+
+import java.util.List;
+import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+import javax.json.JsonValue;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -12,7 +21,10 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import sg.edu.nus.iss.codepirates.snapNshare.ejb.FriendsEJB;
 
 /**
  * REST Web Service
@@ -24,6 +36,9 @@ public class FriendsService {
 
     @Context
     private UriInfo context;
+    
+    @EJB
+    FriendsEJB friendsEJB;
 
     /**
      * Creates a new instance of UserResource
@@ -37,10 +52,21 @@ public class FriendsService {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
-        return "test";
+    @Path("/{username}")
+    public String getFriends(@PathParam("username") String userName) {
+        List<String> friendNames = friendsEJB.getFriends(userName);
+        //System.out.println("here");
+        JsonArrayBuilder nameBuilder = Json.createArrayBuilder();
+        for(String name:friendNames){
+            nameBuilder.add(name);  
+            System.out.println(name);
+        }
+        return nameBuilder.build().toString();
+        //return (Response.ok(nameBuilder, MediaType.APPLICATION_JSON).build());
+        
     }
-
+    
+    
     /**
      * PUT method for updating or creating an instance of UserResource
      * @param content representation for the resource
